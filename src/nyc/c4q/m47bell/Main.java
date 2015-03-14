5fb35738628833c984f052dcf0a0db17470e0c7c
m47bell <m.vidals047@gmail.com>
2015-03-14 10:31:23 -0400

    Initial commit 2 change file name

--- src/nyc/c4q/m47bell/Main.java
+++ src/nyc/c4q/m47bell/Main.java
@@ -1,17 +1,8 @@
 package nyc.c4q.m47bell;
-/* Marbella Vidals
-*Access code 2-1
-*Week 1 Review
-*/
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-
-
-
-
     }
 }
